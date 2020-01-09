3be2abfa08c640dd2879dab7aa93525d69158e91
Lee sang min <dltkd96als@naver.com>
2020-01-09 17:57:38 +0900

    practice

---
+++ mouse_control.py
@@ -0,0 +1,19 @@
+import pyautogui
+import time
+
+# get the mouse coordinates
+print(pyautogui.position())
+
+# use to mouse coordinates and move, this example is Google search bar
+pyautogui.moveTo(677, 468)
+pyautogui.click()
+
+# if you want double click, try this
+# pyautogui.doubleClick()
+# pyautogui.click(clicks=2, interval=sec)
+
+time.sleep(1)
+
+# write your search word
+pyautogui.typewrite("install python")
+pyautogui.typewrite(['enter'])
